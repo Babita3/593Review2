9cf8f4b73308c0551caf427eba8e9bd80585c5f6
Babita3 <babita.sima1996@gmail.com>
2022-09-26 20:40:33 +0530

    First

---
+++ test1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+num1=10
+num2=20
+
+if [ $num1 -gt $num2 ]
+then
+	echo "First value is greater than second value."
+else
+	echo "Second value is greater than first value."
+fi
---
+++ test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+newArray=(0 1 2 3 4 5 6 7 8 9)
+
+echo ${newArray[@]}
+
+
+echo ${newArray[4]}
